7b13ee4e30e268967dd2ab0e2e4c2d0ff262ce00
weitzela1 <weitzel.a1@stu.janesville.k12.wi.us>
2025-02-17 15:03:04 -0600

    Add files via upload

---
+++ Lang122bFor/MainForm.py
@@ -0,0 +1,100 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+    def __init__(self):
+        self.InitializeComponent()
+    
+    def InitializeComponent(self):
+        self._listBox1 = System.Windows.Forms.ListBox()
+        self._button1 = System.Windows.Forms.Button()
+        self._button2 = System.Windows.Forms.Button()
+        self._button3 = System.Windows.Forms.Button()
+        self.SuspendLayout()
+        # 
+        # listBox1
+        # 
+        self._listBox1.BackColor = System.Drawing.Color.Azure
+        self._listBox1.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._listBox1.FormattingEnabled = True
+        self._listBox1.ItemHeight = 25
+        self._listBox1.Location = System.Drawing.Point(12, 12)
+        self._listBox1.Name = "listBox1"
+        self._listBox1.Size = System.Drawing.Size(385, 379)
+        self._listBox1.TabIndex = 0
+        # 
+        # button1
+        # 
+        self._button1.BackColor = System.Drawing.Color.SkyBlue
+        self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button1.Location = System.Drawing.Point(12, 409)
+        self._button1.Name = "button1"
+        self._button1.Size = System.Drawing.Size(118, 72)
+        self._button1.TabIndex = 1
+        self._button1.Text = "Calculate"
+        self._button1.UseVisualStyleBackColor = False
+        self._button1.Click += self.Button1Click
+        # 
+        # button2
+        # 
+        self._button2.BackColor = System.Drawing.Color.DeepSkyBlue
+        self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button2.Location = System.Drawing.Point(146, 409)
+        self._button2.Name = "button2"
+        self._button2.Size = System.Drawing.Size(118, 72)
+        self._button2.TabIndex = 2
+        self._button2.Text = "Clear"
+        self._button2.UseVisualStyleBackColor = False
+        self._button2.Click += self.Button2Click
+        # 
+        # button3
+        # 
+        self._button3.BackColor = System.Drawing.Color.SkyBlue
+        self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+        self._button3.Location = System.Drawing.Point(279, 409)
+        self._button3.Name = "button3"
+        self._button3.Size = System.Drawing.Size(118, 72)
+        self._button3.TabIndex = 3
+        self._button3.Text = "Exit"
+        self._button3.UseVisualStyleBackColor = False
+        self._button3.Click += self.Button3Click
+        # 
+        # MainForm
+        # 
+        self.BackColor = System.Drawing.Color.LightSeaGreen
+        self.ClientSize = System.Drawing.Size(409, 493)
+        self.Controls.Add(self._button3)
+        self.Controls.Add(self._button2)
+        self.Controls.Add(self._button1)
+        self.Controls.Add(self._listBox1)
+        self.Name = "MainForm"
+        self.Text = "Lang122bFor"
+        self.Load += self.MainFormLoad
+        self.ResumeLayout(False)
+
+
+    def Button2Click(self, sender, e):
+        self._listBox1.Items.Clear()
+
+        
+
+    def Button3Click(self, sender, e):
+        Application.Exit()
+
+    def Button1Click(self, sender, e):
+        self._listBox1.Items.Clear()
+        hours = 1
+        pay = hours * 4
+
+        
+        while hours <= 40:
+            self._listBox1.Items.Add(str(hours) + "\t" + str(pay))
+            hours += 1
+            pay += 4
+
+
+    def MainFormLoad(self, sender, e):
+        pass
